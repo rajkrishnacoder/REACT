8963f2d56806dd595d1756778334f18fc34d67b4
Rajkrishna debnath <rajkrishnadebnath53@gmail.com>
2024-07-12 08:32:25 +0530

    fininshing redux project

--- reduxToolkittodo/src/App.jsx
+++ reduxToolkittodo/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
+import AddTodo from './components/addTodo'
+import Todos from './components/Todos'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>hello world</h1>
+      <AddTodo/>
+      <Todos/>
     </>
   )
 }
--- reduxToolkittodo/src/features/todo/todoSlice.js
+++ reduxToolkittodo/src/features/todo/todoSlice.js
@@ -23,5 +23,5 @@ export const todoSlice = createSlice({
 
 
 export const {addTodo, removeTodo} = todoSlice.actions
-
+   
 export default todoSlice.reducer
